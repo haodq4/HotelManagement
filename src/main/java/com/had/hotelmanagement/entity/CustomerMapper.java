de99c5f4be59b4a8638250d9e128125748597911
Hao Duong <haodq4@gmail.com>
2019-10-16 20:35:31 +0700

    haodq

--- src/main/java/com/had/hotelmanagement/entity/CustomerMapper.java
+++ src/main/java/com/had/hotelmanagement/entity/CustomerMapper.java
@@ -10,13 +10,13 @@ import org.springframework.jdbc.core.RowMapper;
 public class CustomerMapper implements RowMapper<Customer>{
 	public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
 		Customer customer = new Customer();
-		customer.setCustomerid(rs.getInt("customerid "));
-		customer.setName(rs.getString("name "));
-		customer.setGovernmentid(rs.getString("governmentid "));
-		customer.setAddress(rs.getString("address "));
-		customer.setPhone(rs.getString("phone "));
-		customer.setEmail(rs.getString("email "));
-		customer.setImage(rs.getString("image "));
+		customer.setCustomerid(rs.getInt("customerid"));
+		customer.setName(rs.getString("name"));
+		customer.setGovernmentid(rs.getString("governmentid"));
+		customer.setAddress(rs.getString("address"));
+		customer.setPhone(rs.getString("phone"));
+		customer.setEmail(rs.getString("email"));
+		customer.setImage(rs.getString("image"));
 		customer.setCcnumber(rs.getInt("ccnumber"));
 		return customer;
 	}
